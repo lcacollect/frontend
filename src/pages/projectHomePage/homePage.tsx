bdfd28226fd7ffaef7174ba5ef9132995ecb0899
Christian Kongsgaard <chrk@arkitema.com>
2023-02-16 10:10:26 +0100

    added options to disable and show tooltip on project create button

---
+++ src/pages/projectHomePage/homePage.tsx
@@ -0,0 +1,27 @@
+import { RecentProjectsPaper, ProjectsTable } from '@lcacollect/project'
+import { PaperPageStack } from '@lcacollect/components'
+import { useMemo } from 'react'
+import { useGetAccountRolesQuery } from '../../dataAccess'
+
+export const ProjectsHomePage = () => {
+  const { data: accountData } = useGetAccountRolesQuery()
+  const isAdmin = useMemo(
+    () => (accountData ? accountData.account.roles.indexOf('lca_super_admin') > -1 : false),
+    [accountData],
+  )
+
+  const canCreateProjects = useMemo(
+    () => isAdmin || (accountData ? accountData.account.roles.indexOf('project.create') > -1 : false),
+    [accountData, isAdmin],
+  )
+
+  return (
+    <PaperPageStack data-testid='projects-homepage'>
+      <RecentProjectsPaper />
+      <ProjectsTable
+        canCreateProjects={canCreateProjects}
+        createButtonToolTip={canCreateProjects ? null : 'Call Molio to get project create rights'}
+      />
+    </PaperPageStack>
+  )
+}
